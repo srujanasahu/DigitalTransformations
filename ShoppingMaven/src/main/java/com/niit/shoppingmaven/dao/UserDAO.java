302a88a1ad224dfcdd13dbcf39bf9a9fbe2c9b68
srujanasahu <srujanasahu198@gmail.com>
2016-06-21 14:31:38 +0000

    Add files via upload

---
+++ ShoppingMaven/src/main/java/com/niit/shoppingmaven/dao/UserDAO.java
@@ -0,0 +1,24 @@
+package com.niit.shoppingmaven.dao;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UserDAO {
+	
+
+	
+	public boolean isValidUser(String userName, String password)
+	{
+		if(userName.equals("NIIT")  && password.equals("NIIT@123")	)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+
+
+}
